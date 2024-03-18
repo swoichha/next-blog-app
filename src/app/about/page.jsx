29d04263b7b1145a7fd2bebb1e0faf539538a0aa
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-18 17:41:58 +0545

    about page design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,15 +1,43 @@
 import Image from "next/image";
-import styles from "./about.modules.css";
+import styles from "./about.module.css";
+
+export const metadata = {
+  title: "About Page",
+  description: "About description",
+};
 
 const AboutPage = () => {
+  // console.log("lets check where it works")
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/19877487/pexels-photo-19877487/free-photo-of-sun-through-massive-redwood-trees-in-forest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-          fill
-        />
+        <Image src="/about.png" alt="About Image" fill className={styles.img} />
       </div>
     </div>
   );
