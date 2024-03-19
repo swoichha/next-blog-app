60bad97ef9921cc2bb6ce0c6a8aa27040d9e3cf8
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-19 11:12:06 +0545

    Contact page design

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,29 @@
+import styles from "./contact.module.css";
+import Image from "next/image";
+
 const ContactPage = () => {
-  return <div>Contact Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="First Name and Last Name" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            row="30"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
