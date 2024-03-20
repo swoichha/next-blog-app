6988b35eb91a18f6fd58479265fd09d67505b169
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-20 18:18:27 +0545

    changed the logo name

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Lamadev</div>
+      <div className={styles.logo}>Blog Post</div>
       <div className={styles.text}>
         Create thoughts Agency © All rights reserved.
       </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
-        Logo
+        Blog Post
       </Link>
       <div>
         <Links session={session} />
